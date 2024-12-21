aac50d5a2bf223d00d336f7f8f8a12ad9675e7a6
Cyril-hub-76 <desig.on.web.76@gmail.com>
2024-12-21 23:01:30 +0100

    création du middleware d'authentification

---
+++ middlewares/auths.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+// verify if user is loged
+module.exports = (req, resp, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, "MIIEvQIBADANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQ");
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId : userId
+        };
+    } catch (error) {
+        resp.status(401).json({ error });
+    }
+}
