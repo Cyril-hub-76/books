2b8d8cf9c7788c76cd34faf8f93a001a5b5360de
Cyril-hub-76 <desig.on.web.76@gmail.com>
2025-01-10 14:23:20 +0100

    status code

--- routes/book.js
+++ routes/book.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+// token transmiter
 const auth = require("../middlewares/auths");
 const multer = require("../middlewares/multer-config");
 const bookCtrl = require("../controllers/book");
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,10 @@
 const express = require("express");
-// token transmiter
-const auth = require("../middlewares/auths");
+
 //router from express implementation
 const router = express.Router();
 
 const userController = require("../controllers/user");
 
-// dont forget to add "auth" transmiter to routers
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 module.exports = router;
